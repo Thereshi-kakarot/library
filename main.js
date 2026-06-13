bc3e8f00cf72a05db1fbefe3875385d8b6905efb
Ngoepe Johnny Thereshi <ngoepejohnny1@gmail.com>
2026-06-13 19:25:14 +0200

    I have written down my JavaScript pseudocode for the functionality of ny library project

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 /* 
 What I am going to start with is to make sure that I link all required elements needed to be worked with from HTML to JavaScript.
 Then I will make the first button work by letting it show and hide the form.
-The form will 
+The form inputs will also be linked to the JavaScript code so that when the form is submitted, the data will be captured and displayed as an HTML card div 
+I will have an empty array of Library to store the data of the books that will be added to the library from form inputs.
+I will have a function called book which will be a constructor function to create a book object with data from form inputs.
+I will also have a function called addBookToLibrary which will push the book object to the library array and display it as an HTML card div.
 
 */
+
